34969d9246ff9d911a2f20af61243125dfb152dd
joaolima7 <jcgam3r69@gmail.com>
2025-04-29 20:02:51 -0300

    Add configuration files and load configuration in main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "github.com/joaolima7/api1_goexpert/configs"
 
+func main() {
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic("could not load config")
+	}
+	println(config.DBDriver)
 }
