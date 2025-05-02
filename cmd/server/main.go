add9662b5c559e8bf79f9c33f00db47fc4e9a203
joaolima7 <jcgam3r69@gmail.com>
2025-05-02 20:03:56 -0300

    Implement Product creation handler and DTO; update ProductInterface methods for consistency

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,65 @@
 package main
 
-import "github.com/joaolima7/api1_goexpert/configs"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/joaolima7/api1_goexpert/configs"
+	"github.com/joaolima7/api1_goexpert/internal/dto"
+	"github.com/joaolima7/api1_goexpert/internal/entity"
+	"github.com/joaolima7/api1_goexpert/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, err := configs.LoadConfig(".")
+	_, err := configs.LoadConfig(".")
 	if err != nil {
 		panic("could not load config")
 	}
-	println(config.DBDriver)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	productDB := database.NewProduct(db)
+	productHandler := NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductRequest
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.CreateProduct(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
